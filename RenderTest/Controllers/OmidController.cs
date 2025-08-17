cbf7ffb9e2ec89f9a4f90963ef44e74f5d1bf77c
o.dadvar <o.dadvar@amnmoj.local>
2025-08-17 09:47:27 +0330

    initial commit

---
+++ RenderTest/Controllers/OmidController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RenderTest.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OmidController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("Omid's Test Passed :D !!!");
+    }
+}
