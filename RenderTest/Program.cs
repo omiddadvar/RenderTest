14908f22e83508b6cd2ed6beb5c507a70f713a7e
o.dadvar <o.dadvar@amnmoj.local>
2025-09-06 12:23:38 +0330

    feat : Added-Swagger

--- RenderTest/Program.cs
+++ RenderTest/Program.cs
@@ -1,11 +1,19 @@
 using RenderTest;
 using RenderTest.Middlewares;
+using Microsoft.OpenApi.Models; // Add this using
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add OpenAPI/Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RenderTest API", Version = "v1" });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,6 +24,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "RenderTest API v1");
+    });
+
     app.MapOpenApi();
 }
 
