7ec32852ddb3293ccc8417e3707d0ecff0410277
o.dadvar <o.dadvar@amnmoj.local>
2025-09-06 12:24:33 +0330

    refactor : Added-Swagger-To-All-Types-Of-productions

--- RenderTest/Program.cs
+++ RenderTest/Program.cs
@@ -4,25 +4,22 @@ using Microsoft.OpenApi.Models; // Add this using
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Add OpenAPI/Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "RenderTest API", Version = "v1" });
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.AddServiceConfigurations();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+if (true)
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
