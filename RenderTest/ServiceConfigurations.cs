04a7d73325f96a775e8e4646bd39847d3b9b8c54
o.dadvar <o.dadvar@amnmoj.local>
2025-09-09 11:25:24 +0330

    feat : Added-IConnectionMultiplexer-AddSingleton

--- RenderTest/ServiceConfigurations.cs
+++ RenderTest/ServiceConfigurations.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using RenderTest.Abstractions.Services;
 using RenderTest.Data;
 using RenderTest.Services;
+using StackExchange.Redis;
 
 public static class ServiceConfigurations
 {
@@ -34,6 +35,17 @@ public static class ServiceConfigurations
         {
             options.Configuration = builder.Configuration.GetConnectionString("redis");
             options.InstanceName = "RenderTest:"; // Optional: prefix for all keys
+        });
+
+        builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("redis"));
+            configuration.AbortOnConnectFail = false;
+            configuration.ConnectRetry = 3;
+            configuration.ConnectTimeout = 5000;
+            configuration.SyncTimeout = 5000;
+
+            return ConnectionMultiplexer.Connect(configuration);
         });
 
         builder.Services.AddScoped<IRedisService, RedisService>();
